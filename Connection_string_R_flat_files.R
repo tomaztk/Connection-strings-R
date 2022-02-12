f25a5761f17e938525c1851d5263bcb1f34254cf
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2022-02-12 21:23:15 +0100

    update and adding database

--- Connection_string_R_flat_files.R
+++ Connection_string_R_flat_files.R
@@ -8,7 +8,9 @@
 ################################
 
 #Set working directory
-setwd("/Users/tomazkastrun/Documents/tomaztk_github/Connection-strings-R/data/flat_file")
+#setwd("/Users/tomazkastrun/Documents/tomaztk_github/Connection-strings-R/data/flat_file") #unix
+setwd("c://datatk//git//connection-strings-r//data//flat_file")  #windows
+
 
 
 # Read a txt file, reading data from sample.txt file
@@ -36,3 +38,22 @@ listviewer::jsonedit(data_json, height = "400px", mode = "view")
 
 
 # Database
+#install.packages("RSQLite")
+library(RSQLite)
+
+# point path to folder
+#setwd("/Users/tomazkastrun/Documents/tomaztk_github/Connection-strings-R/data/database") #unix
+setwd("c://datatk//git//connection-strings-r//data//database") #windows
+sqlite <- dbDriver("SQLite")
+conn <- dbConnect(sqlite,"sampledb.db")
+
+# Get the table Name
+dbListTables(conn)
+
+#get into Data.frame  
+data_from_db <- data.frame(dbGetQuery(conn, "SELECT * FROM data_db"))
+
+head(data_from_db)
+
+#use query
+dbGetQuery(conn, "SELECT * FROM data_db WHERE age > 30")
